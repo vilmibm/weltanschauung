6a46cf2972f7d5c20ad3be4468518a6979c841a0
Nathaniel Smith <nathanielksmith@gmail.com>
2009-12-03 21:47:25 -0500

    Add new rule modules

---
+++ code/Rule/Keyword/Exact.pm
@@ -0,0 +1,37 @@
+package Rule::Keyword::Exact;
+use base 'Rule';
+
+
+use strict;
+use warnings;
+
+use feature 'switch';
+
+sub new {
+    my $class = shift;
+    my $self  = {};
+
+    $self->{keyword}  = shift || die 'must pass a keyword';
+    $self->{weakness} = 11;
+
+    bless $self, $class;
+
+    return $self;
+}
+
+sub get_clause {
+    my $self = shift;
+
+    my $keyword = $self->{keyword};
+
+    given ($self->get_weakness()) {
+        when (11) { return "(sentence LIKE '%$keyword%')" }
+        when (1..10) {
+            my $range = 11 - $_;
+            return "(line_no in (SELECT line_no+$range, line_no-$range FROM lines WHERE sentence LIKE '%$keyword%'))";
+        }
+        when (0) { return '(1)' }
+    }
+}
+
+1;
